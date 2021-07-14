519dcf718e0b395b9cec80a62004fbbd8530bf65
Rafael Hengles <rhengles@gmail.com>
2021-07-14 11:44:06 -0300

    do not create folder in dist

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
 const config = require('./vite.config.min')
-const pkgName = require('./package.json').name
+const pkgName = require('./package.json').name.replace(/^@arijs\//i,'')
 
 config.build.lib.fileName = pkgName;
 config.build.minify = false;
